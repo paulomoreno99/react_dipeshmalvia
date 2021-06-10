80459151493f65e62570871c89fe0d9ae2a80a83
paulomoreno99 <paulom.info@gmail.com>
2021-06-10 18:18:34 -0300

    components_23:20

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+function App() {
+
+const contacts = [
+  {
+    id:"1",
+    name:"Paulo",
+    email:"paulom.info@gmail.com"
+},
+{
+  id:"2",
+  name:"Bily",
+  email:"bily@gmail.com"
+}]
+
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact />
+     <ContactList contacts={contacts}/>
+    </div>
+  );
+}
+
+export default App;
