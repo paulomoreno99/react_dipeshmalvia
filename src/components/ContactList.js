dc4341ff84592204551038a3d15a2127785171db
paulomoreno99 <paulom.info@gmail.com>
2021-06-11 05:57:54 -0300

    before hooks_29:20

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,17 +1,15 @@
 import React from 'react'
+import ContactCard from './ContactCard'
+
+
 
 const ContactList = (props) => {
-    console.log(props)
+      
 
     const renderContactList = props.contacts.map((contact) => {
         return (
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className="trash alternate outline icon"></i>
-            </div>
+           <ContactCard contact={contact}></ContactCard>
+            
         )
     })
     return <div className="ui celled list">{renderContactList}</div>
@@ -19,3 +17,17 @@ const ContactList = (props) => {
 }
 
 export default ContactList
+
+
+
+
+/*<table>
+              <tr>
+                <th>Nome</th>
+                <th>Email</th>
+              </tr>
+              <tr>
+                <td>{contact.name}</td>
+                <td>{contact.email}</td>
+              </tr>
+            </table>*/
